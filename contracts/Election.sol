2aaad070e62d5fce9869f45fd00f4bf77b350dc6
Douglas B <guod1977@gmail.com>
2022-02-16 14:59:49 -0800

    First commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    //Store Candidate
+    //Read Candidate
+    string public candidate;
+    //Contstructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+
+}
